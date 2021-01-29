687f8ceb969745ba7eb32d7679a2ef9c1f513c54
Jason Gibson <71955479+jgibsone4@users.noreply.github.com>
2021-01-28 21:06:21 -0500

    saving for eve

--- index.js
+++ index.js
@@ -1,92 +1,94 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const util = require('util');           
-const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
+const generateMarkdown = require("./utils/generateMarkdown.js");
+// TODO: Create an array of questions for user input
 const questions = [
-    {
-      type: 'confirm',
-      name: 'start',
-      message: 'This will generate a README file after you enter some information',
-    },
-    {
-      type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
-    },
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'What is your email address?',
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username?',
-    },
-    {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL?',
-    },
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Application Title:',
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Description:',
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: 'Installation instuctions:',
-      },
-      {
-        type: 'input',
-        name: 'credits',
-        message: 'Credits:',
-      },
-      {
-        type: 'input',
-        name: 'contributing',
-        message: 'Contributing:',
-      },
-      {
-        type: 'input',
-        name: 'code of conduct',
-        message: 'Code of Conduct:',
-      },
-      {
-        type: 'list',
-        name: 'license',
-        Choices: ["MIT", "BSD2", "BSD3", "Apache 2.0"],
-        message: "Select your license."
-    }
+  {
+    type: "confirm",
+    name: "start",
+    message:
+      "This will generate a README file after you enter some information",
+  },
+  {
+    type: "input",
+    name: "location",
+    message: "Where are you from?",
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub Username?",
+  },
+  {
+    type: "input",
+    name: "linkedin",
+    message: "Enter your LinkedIn URL?",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "Application Title:",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Description:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Installation instuctions:",
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "Credits:",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Contributing:",
+  },
+  {
+    type: "input",
+    name: "code of conduct",
+    message: "Code of Conduct:",
+  },
+  {
+    type: "list",
+    name: "license",
+    Choices: ["MIT", "BSD2", "BSD3", "Apache 2.0"],
+    message: "Select your license.",
+  },
 ];
-
-  function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-      if (err) new Error(err);
-      console.log("README successfully created!");
-    });
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) new Error(err);
+    console.log("Perfect");
+  });
+}
+// TODO: Create a function to initialize app
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      writeToFile("test.md", generateMarkdown(answers));
+    })
+    .catch((err) => console.log(err));
 }
 
-  function init() {
-    inquirer.prompt(promptUser)
-      .then(answers => {
-        writeToFile("test.md", generateMarkdown(answers))
-      })
-      .catch(err => console.log(err));
-  }
-  
-
+// Function call to initialize app
 init();
